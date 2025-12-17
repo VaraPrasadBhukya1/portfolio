43d26a879b193435e66ab422073d8d28c9000a90
Varaprasad <varaprasad@example.com>
2025-12-17 14:31:11 +0530

    added hosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'www.way2studios.com',
-      'way2studios.com',
-      'reelreporter.way2news.com',
-      'www.reelreporter.way2news.com',
-      'unreprievable-redundantly-krissy.ngrok-free.dev'
+      '*',
     ],
   }
 })
